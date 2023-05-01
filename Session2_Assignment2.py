3183eb991d3524111bde40035cec0f5b654a5b6e
LalehMashayekhi <132183349+LalehMashayekhi@users.noreply.github.com>
2023-05-01 21:30:56 +0330

    Update Session2_Assignment2.py

--- Session2_Assignment2.py
+++ Session2_Assignment2.py
@@ -10,7 +10,7 @@ average= (course_1 + course_2 + course_3)/3
 
 if average >= 17:
      print(full_name,' Statuse: Greate ')
-elif average < 17 and average>12:
+elif average < 17 and average>=12:
     print(full_name, ' Statuse: Normal ')
 elif average<12:
     print(full_name, ' Statuse: Fail ')
